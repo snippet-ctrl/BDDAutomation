2a3f537e733cf583395aaffa36dc0de6f458fd93
snippet-ctrl <Jaydeepsingh1897@gmail.com>
2025-08-16 11:19:32 -0400

    Add files via upload

---
+++ src/test/java/Feature/login/login.feature
@@ -0,0 +1,6 @@
+Feature: To Test Login Functionality
+  Scenario:
+    Given user is on login page
+    When user enters username and Password
+    And clicks on login button
+    Then user is navigated to Home Screen
---
+++ src/test/java/Steps/LoginSteps.java
@@ -0,0 +1,27 @@
+package Steps;
+
+import io.cucumber.java.en.*;
+
+
+public class LoginSteps {
+    @Given("user is on login page")
+    public void userIsOnLoginPage()  {
+        System.out.println("On Login");
+    }
+
+    @When("user enters username and Password")
+    public void userEntersUsernameAndPassword() {
+        System.out.println("Entered UName & Password");
+    }
+
+    @And("clicks on login button")
+    public void clicksOnLoginButton() {
+        System.out.println("Clicked Login Button");
+
+    }
+
+    @Then("user is navigated to Home Screen")
+    public void userIsNavigatedToHomeScreen() {
+        System.out.println("I'm at Home Screen");
+    }
+}
