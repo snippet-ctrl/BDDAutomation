552c7c795f1afb86d46592d1802e5a3966d4c75e
snippet-ctrl <jaydeepsingh1897@gmail.com>
2025-08-29 05:20:04 -0400

    RMS-123 File names changed

--- src/test/java/RunnerDemo/LoginRunner.java
+++
@@ -1,9 +0,0 @@
-package RunnerDemo;
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.Cucumber;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(features = {"C:\\Users\\jayde\\IdeaProjects\\GitHubDemo\\src\\test\\java\\Feature\\login"}, glue = {"Steps"},dryRun = false)
-public class LoginRunner {
-}
